b299db6ae6561126787cfb10e3545f457372cc23
Sabithasan2098 <sabithasan2098@gmail.com>
2025-08-23 21:21:46 +0600

    responsive footer

--- src/components/shered/footer/Footer.tsx
+++ src/components/shered/footer/Footer.tsx
@@ -1,41 +1,44 @@
 import React from "react";
+import "./Footer.css";
+import Link from "next/link";
 
 const Footer = () => {
   return (
-    <div className="max-w-[1920px] mx-auto text-white mt-[130px]">
-      <div className="flex items-center h-[400px] ">
+    <div className="max-w-[1920px] mx-auto text-white mt-[30px] md:mt-[50px] lg:mt-[130px]">
+      <div className="flex items-center h-[170px] md:h-[250px] lg:h-[400px] ">
         <div className="bg-[#1F2937]  h-full flex-1 relative">
-          <div className="flex flex-col w-[345px] top-[97px] items-center absolute left-[457px]">
-            <h1 className="uppercase text-[32px] font-medium pb-6">
+          <div className="flex flex-col lg:w-[345px] md:top-[30px] lg:top-[97px] items-center absolute md:left-[97px] lg:left-[457px] py-5">
+            <h1 className="uppercase text-[18px] md:text-[24px] lg:text-[32px] font-medium pb-2 md:pb-[14px] lg:pb-6">
               contact us
             </h1>
-            <p className="text-[20px] leading-[36px] font-medium">
-              123 ABS Street, Uni 21, Bangladesh
+            <p className="text-[12px] md:text-[16px] text-center lg:text-[20px] lg:leading-[36px] lg:font-medium">
+              123 ABS Street, Uni 21, Bangladesh.
             </p>
-            <p className="text-[20px] leading-[36px] font-medium">
+            <p className="text-[12px] md:text-[16px] text-center lg:text-[20px] lg:leading-[36px] lg:font-medium">
               +88 123456789
             </p>
-            <p className="text-[20px] leading-[36px] font-medium">
+            <p className="text-[12px] md:text-[16px] text-center lg:text-[20px] lg:leading-[36px] lg:font-medium">
               Mon - Fri: 08:00 - 22:00
             </p>
-            <p className="text-[20px] leading-[36px] font-medium">
+            <p className="text-[12px] md:text-[16px] text-center lg:text-[20px] lg:leading-[36px] lg:font-medium">
               Sat - Sun: 10:00 - 23:00
             </p>
           </div>
         </div>
         <div className=" bg-[#111827] h-full flex-1 relative">
-          <div className="flex flex-col w-[345px] top-[97px] items-center absolute right-[457px]">
-            <h1 className="text-[32px]  font-medium pb-6">Follow Us</h1>
-            <p className="text-[20px] leading-[36px] font-medium">
+          <div className="flex flex-col lg:w-[470px] md:top-[30px] lg:top-[97px] items-center absolute md:left-[97px] lg:right-[457px] left-8 py-5">
+            <h1 className="text-[18px] md:text-[24px] lg:text-[32px]  lg:font-medium pb-2 md:pb-[14px] lg:pb-6">
+              Follow Us
+            </h1>
+            <p className="text-[12px] md:text-[16px] text-center lg:text-[20px] lg:leading-[36px] font-medium">
               Join us on social media
             </p>
-            <div className="pt-8 flex  items-center gap-8">
+            <div className="pt-2 md:pt-4 lg:pt-8 flex  items-center gap-2 md:gap-4 lg:gap-8">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
-                width="35"
-                height="35"
                 viewBox="0 0 35 35"
                 fill="none"
+                className="w-5 h-5 md:w-7 md:h-7 lg:w-10 lg:h-10 fill-white"
               >
                 <path
                   d="M20.2105 35V19.036H25.5668L26.3704 12.8127H20.2105V8.84007C20.2105 7.03885 20.7087 5.81134 23.2945 5.81134L26.5872 5.80998V0.243652C26.0178 0.169654 24.0632 0 21.7882 0C17.0376 0 13.7853 2.89969 13.7853 8.22372V12.8127H8.41278V19.036H13.7853V35H20.2105Z"
@@ -44,10 +47,9 @@ const Footer = () => {
               </svg>
               <svg
                 xmlns="http://www.w3.org/2000/svg"
-                width="35"
-                height="35"
                 viewBox="0 0 35 35"
                 fill="none"
+                className="w-5 h-5 md:w-7 md:h-7 lg:w-10 lg:h-10 fill-white"
               >
                 <path
                   d="M25.342 0H9.63105C4.27748 0 0 4.27748 0 9.63105V25.3689C0 30.6956 4.27748 35 9.63105 35H25.3689C30.6956 35 35 30.7225 35 25.3689V9.63105C34.9731 4.27748 30.6956 0 25.342 0ZM31.9062 23.97C31.9062 28.3282 28.3551 31.9331 23.9431 31.9331H11.03C6.67179 31.9331 3.06687 28.382 3.06687 23.97V11.03C3.06687 6.67179 6.61799 3.06687 11.03 3.06687H24.0238C28.382 3.06687 31.9869 6.61799 31.9869 11.03V23.97H31.9062Z"
@@ -64,10 +66,9 @@ const Footer = () => {
               </svg>
               <svg
                 xmlns="http://www.w3.org/2000/svg"
-                width="35"
-                height="35"
                 viewBox="0 0 35 35"
                 fill="none"
+                className="w-5 h-5 md:w-7 md:h-7 lg:w-10 lg:h-10 fill-white"
               >
                 <path
                   d="M35 6.64781C33.6984 7.21875 32.3116 7.59719 30.8656 7.78094C32.3531 6.89281 33.4884 5.49719 34.0222 3.815C32.6353 4.64187 31.1041 5.22594 29.4722 5.55188C28.1553 4.14969 26.2784 3.28125 24.2309 3.28125C20.2584 3.28125 17.0603 6.50563 17.0603 10.4584C17.0603 11.0272 17.1084 11.5741 17.2266 12.0947C11.2612 11.8038 5.98281 8.94469 2.43687 4.58938C1.81781 5.66344 1.45469 6.89281 1.45469 8.21625C1.45469 10.7013 2.73438 12.9041 4.64187 14.1794C3.48906 14.1575 2.35812 13.8228 1.4 13.2956C1.4 13.3175 1.4 13.3459 1.4 13.3744C1.4 16.8612 3.88719 19.7575 7.14875 20.4247C6.56469 20.5844 5.92812 20.6609 5.2675 20.6609C4.80813 20.6609 4.34437 20.6347 3.90906 20.5384C4.83875 23.38 7.47688 25.4691 10.6138 25.5369C8.1725 27.4466 5.07281 28.5972 1.71719 28.5972C1.12875 28.5972 0.564375 28.5709 0 28.4987C3.17844 30.5484 6.94531 31.7188 11.0075 31.7188C24.2113 31.7188 31.43 20.7812 31.43 11.3006C31.43 10.9834 31.4191 10.6772 31.4037 10.3731C32.8278 9.3625 34.0244 8.10031 35 6.64781Z"
@@ -78,9 +79,11 @@ const Footer = () => {
           </div>
         </div>
       </div>
-      <p className="bg-[#151515] py-[17px] flex justify-center">
-        Copyright © CulinaryCloud. All rights reserved.
-      </p>
+      <Link href={"https://sabit-portfolio-qq44.vercel.app/"}>
+        <p className="bg-[#151515] py-[7px] md:py-[12px] lg:py-[17px] flex justify-center custom-text text-[16px] md:text-[22px] lg:text-[26px] font-medium md:font-semibold ">
+          Developer: @ S a b i t H a s a n
+        </p>
+      </Link>
     </div>
   );
 };
